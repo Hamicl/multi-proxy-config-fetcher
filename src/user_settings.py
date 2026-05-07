a516d9abcce497b8002428347b89a8af0e40e2b3
Hamid <hamicl.r@gmail.com>
2026-05-07 10:40:27 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -58,6 +58,7 @@ SOURCE_URLS = [
     "https://t.me/s/kingproxiy",
     "https://t.me/s/proxymtpvpn",
     "https://t.me/s/proxymtprotoir",
+    "https://t.me/s/farah_vpn",
     "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_1.txt",
     "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_2.txt",
     "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_3.txt",
